5f6fda3392b0fd3703007901486cee79b8b12abc
Wonjeong <wonjeong@huliac.com>
2026-09-15 14:48:13 +0900

    패키지 업데이트 및 GameManager 수정

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -12,7 +12,7 @@ namespace Core
     /// 싱글톤 유지, 디버그 토글, Settings 비동기 로드는 GameManagerBase가 제공하며
     /// 프로젝트 고유의 전역 흐름 제어를 이곳에 추가함.
     /// </summary>
-    public class GameManager : GameManagerBase<GameManager>
+    public class GameManager : GameManagerBase
     {
         private ISubscriber<InactivityTimeoutEvent> _inactivityTimeoutSubscriber;
         private IDisposable _inactivityTimeoutSubscription;
